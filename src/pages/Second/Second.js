4e1b3f6e679807e4264fe52e0ed87094afa6b095
RH <1227162636@qq.com>
2018-07-31 20:48:53 +0800

    2018-07-31 20:48:45
    2018-07-31 20:48:48

--- src/pages/Second/Second.js
+++ src/pages/Second/Second.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import {NavLink, withRouter} from "react-router-dom";
 import {connect} from "react-redux";
-import {getAction} from '../../utills/axios'
+
+import { Layout } from 'antd';
+
+const { Header, Sider, Content } = Layout;
 
 class Second extends React.Component{
-    componentDidMount() {
-        getAction("https://malsup.github.io/min/jquery.form.min.js").then(function (response){
-            console.log(response)
-        })
-    }
     render () {
         return (
-            <div>
-                <h4>THIS IS Second PAGE </h4>
-                <p>
-                    <NavLink exact to="/">to home page</NavLink>
-                </p>
-            </div>
+            <Layout>
+                <Header style={{ position: 'fixed', zIndex: 2, width: '100%' }}>
+                    <div style={{width:"100%", height: "64px", background: "white"}}>top</div>
+                </Header>
+                <Layout>
+                    <Sider style={{zIndex: 1, height: '100vh', position: 'fixed', left: 0,  marginTop: 64  }}>
+                        <div style={{width:"100%",height:"100%", background: "white"}}>123</div>
+                    </Sider>
+                    <Content style={{ padding: '0 50px', marginTop: 64 }}>
+                        <h4>THIS IS Second PAGE </h4>
+                        <p>
+                            <NavLink exact to="/">to home page</NavLink>
+                        </p>
+                    </Content>
+                </Layout>
+            </Layout>
         )
     }
 }
